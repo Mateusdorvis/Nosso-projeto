d51cf5787f9a05af0c57327f6d6042b610057a5f
Mateusdorvis <mateusdorvis004@gmail.com>
2024-06-24 15:52:55 -0300

    ola

--- login.py
+++ login.py
@@ -3,8 +3,7 @@ class Login:
         self.nome_do_user = nome_do_user
         self.senha_do_user = senha_do_user
         self.data = data
-
-    
+        
     def __repr__(self):
-        return f"Nome o usuario : {self.nome_do_user}, senha do usuario : {self.senha_do_user}"
+        return f"Nome o usuario : {self.nome_do_user}, senha do usuario : {self.senha_do_user}, data de nascimento : {self.data}"
         
