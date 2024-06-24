59b309fbd68ecc8326a6e5265418de656754fcba
Mateusdorvis <mateusdorvis004@gmail.com>
2024-06-24 15:50:58 -0300

    ok

--- login.py
+++ login.py
@@ -1,3 +1,10 @@
-print("teste")
-print("olá")
-print("ola")
+class Login:
+    def __init__(self, nome_do_user, senha_do_user, data):
+        self.nome_do_user = nome_do_user
+        self.senha_do_user = senha_do_user
+        self.data = data
+
+    
+    def __repr__(self):
+        return f"Nome o usuario : {self.nome_do_user}, senha do usuario : {self.senha_do_user}"
+        
