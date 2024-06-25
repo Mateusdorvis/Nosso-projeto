d86d9bb4c1d4bb805688cff8818de48b8621f71b
Jhedder <jheddersenac@gmail.com>
2024-06-25 15:29:57 -0300

    .

--- login.py
+++ login.py
@@ -1,19 +1,4 @@
-"""class Login:
-    def __init__(self, nome_do_user, senha_do_user, data, cpf):
-        self.nome_do_user = nome_do_user
-        self.senha_do_user = senha_do_user
-        self.data = data
-        self.cpf = cpf
 
-    def exibir(self):
-        print(f"Nome o usuario : {self.nome_do_user}, senha do usuario : {self.senha_do_user}, data de nascimento : {self.data}, CPF : {self.cpf}")
-
-pergunta1 = input(" nome do user :")
-pergunta2 = input("senha do user")
-pergunta3 = int(input("data de nascimento :"))
-pergunta4 = int(input("cpf :"))
-login = Login(pergunta1, pergunta2, pergunta3, pergunta4)
-login.exibir()"""
         
 
 def dados_funcionarios(nome: str, senha: int):
