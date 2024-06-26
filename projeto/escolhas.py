fd94ae09939956666d557008cb4f51cc7bea8f3b
Mateusdorvis <mateusdorvis004@gmail.com>
2024-06-26 13:54:10 -0300

    ok

--- projeto/escolhas.py
+++ projeto/escolhas.py
@@ -1,23 +1,23 @@
 
 def escolhas():
-	escolhasala = {"Forma de pagamento 1": "Sala VIP", "Forma de pagamento 2": "Sala convencional", "Forma de pagamento 3" : "Sala 3D,", "Forma de pagamento  4": "drive-in","Forma de pagamento  5": "Sala IMAX" }
+	escolhasala = {"Escolha de sala 1": "Sala VIP", "Escolha de sala  2": "Sala convencional", "Escolha de sala 3" : "Sala 3D,", "Escolha de sala  4": "drive-in","Escolha de sala  5": "Sala IMAX" }
 	
 	pergunta3 =int(input(f"A seguir escolha qual tipo de sala que queira : {escolhasala} "))
 	
 	if pergunta3==1:
-			tira = escolhasala.get("Forma de pagamento 1")
+			tira = escolhasala.get("Escolha de sala 1")
 			print(f"vc escolheu {tira} ")
 	elif pergunta3==2:
-			tira = escolhasala.get("Forma de pagamento 2")
+			tira = escolhasala.get("FEscolha de sala 2")
 			print(f"vc escolheu  {tira}")
 	elif pergunta3==3:
-			tira = escolhasala.get("Forma de pagamento 3")
+			tira = escolhasala.get("Escolha de sala 3")
 			print(f"vc escolheu {tira} ")
 	elif pergunta3==4:
-			tira = escolhasala.get("Forma de pagamento 4")
+			tira = escolhasala.get("Escolha de sala 4")
 			print(f"vc escolheu {tira} ")
 	elif pergunta3==5:
-			tira = escolhasala.get("Forma de pagamento 5")
+			tira = escolhasala.get("Escolha de sala 5")
 			print(f"vc escolheu  {tira} ")
 	else:
 			print("inválido")
--- projeto/pagamento.py
+++ projeto/pagamento.py
@@ -12,8 +12,11 @@ class Pagamentocinema:
     def gerar_contaprapagar(self):
         codigo_padrao = 12345678901
         print(f"Tipo de pagamento : {self.pagamento},  código do pagamento : {codigo_padrao}, Valor pago : {self.valor}, filme escolhido : {self.filme}, assento escolhido : {self.n_assento},\  dia escolhido : {self.dia}, hora escolhida : {self.hora}")
-        
+
     def tipopagamento(self):
+        escol = {"Forma de pagamento 1": "Sala VIP", "Forma de pagamento 2": "Sala convencional", "Forma de pagamento 3" : "Sala 3D,", "Forma de pagamento  4": "drive-in","Forma de pagamento  5": "Sala IMAX" }
+        pergunta =int(input(f"A seguir escolha qual tipo de sala que queira : {escolhasala} "))
+	
 
 
 #o que está dentro do parametro é argumento para o parametro código
