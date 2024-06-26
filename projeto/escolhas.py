aa65caa3e9c93e83ceeaad954df70c2fa2dabd80
Mateusdorvis <mateusdorvis004@gmail.com>
2024-06-26 13:51:50 -0300

    ok

--- projeto/escolhas.py
+++ projeto/escolhas.py
@@ -2,7 +2,7 @@
 def escolhas():
 	escolhasala = {"Forma de pagamento 1": "Sala VIP", "Forma de pagamento 2": "Sala convencional", "Forma de pagamento 3" : "Sala 3D,", "Forma de pagamento  4": "drive-in","Forma de pagamento  5": "Sala IMAX" }
 	
-	pergunta3 =int(input(f"A seguir escolha qual tipo de sala que queira : {tipopagamento} "))
+	pergunta3 =int(input(f"A seguir escolha qual tipo de sala que queira : {escolhasala} "))
 	
 	if pergunta3==1:
 			tira = escolhasala.get("Forma de pagamento 1")
