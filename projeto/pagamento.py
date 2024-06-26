4da2c274b1508b09a6b9dd3261c98321d46f67bc
Mateusdorvis <mateusdorvis004@gmail.com>
2024-06-26 14:11:53 -0300

    ok

--- projeto/pagamento.py
+++ projeto/pagamento.py
@@ -13,22 +13,6 @@ class Pagamentocinema:
         codigo_padrao = 12345678901
         print(f"Tipo de pagamento : {self.pagamento},  código do pagamento : {codigo_padrao}, Valor pago : {self.valor}, filme escolhido : {self.filme}, assento escolhido : {self.n_assento},\  dia escolhido : {self.dia}, hora escolhida : {self.hora}")
 
-    def tipopagamento():
-        escolhapagamento = {"Forma de pagamento 1": "Pix", "Forma de pagamento 2": "Cartão de crédito", "Forma de pagamento 3" : "Cartão de débito", "Forma de pagamento  4": "Boleto" }
-        paga =int(input(f"A seguir escolha qual tipo de sala que queira : {escolhapagamento} "))
-        if paga==1:
-            tira2 = escolhapagamento.get("Forma de pagamento 1")
-            print(f"você escolheu como forma de pagamento via {tira2}")
-        elif paga==2:
-            tira2 = escolhapagamento.get("Forma de pagamento 2")
-            print(f"você escolheu como forma de pagamento via {tira2}")
-        elif paga==3:
-            tira2 = escolhapagamento.get("Forma de pagamento 3")
-            print(f"você escolheu como forma de pagamento via {tira2}")
-        elif paga==4:
-            tira2 = escolhapagamento.get("Forma de pagamento 4")
-            print(f"você escolheu como forma de pagamento via {tira2}")
-    tipopagamento()
 
 
 
