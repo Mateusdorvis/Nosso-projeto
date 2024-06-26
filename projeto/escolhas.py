bf6759fb52b2d7bce3db50cfa4102172f7ae1482
Mateusdorvis <mateusdorvis004@gmail.com>
2024-06-26 13:51:35 -0300

    ok

--- projeto/escolhas.py
+++ projeto/escolhas.py
@@ -1,23 +1,23 @@
 
 def escolhas():
-	tipopagamento = {"Forma de pagamento 1": "Sala VIP", "Forma de pagamento 2": "Sala convencional", "Forma de pagamento 3" : "Sala 3D,", "Forma de pagamento  4": "drive-in","Forma de pagamento  5": "Sala IMAX" }
+	escolhasala = {"Forma de pagamento 1": "Sala VIP", "Forma de pagamento 2": "Sala convencional", "Forma de pagamento 3" : "Sala 3D,", "Forma de pagamento  4": "drive-in","Forma de pagamento  5": "Sala IMAX" }
 	
 	pergunta3 =int(input(f"A seguir escolha qual tipo de sala que queira : {tipopagamento} "))
 	
 	if pergunta3==1:
-			tira = tipopagamento.get("Forma de pagamento 1")
+			tira = escolhasala.get("Forma de pagamento 1")
 			print(f"vc escolheu {tira} ")
 	elif pergunta3==2:
-			tira = tipopagamento.get("Forma de pagamento 2")
+			tira = escolhasala.get("Forma de pagamento 2")
 			print(f"vc escolheu  {tira}")
 	elif pergunta3==3:
-			tira = tipopagamento.get("Forma de pagamento 3")
+			tira = escolhasala.get("Forma de pagamento 3")
 			print(f"vc escolheu {tira} ")
 	elif pergunta3==4:
-			tira = tipopagamento.get("Forma de pagamento 4")
+			tira = escolhasala.get("Forma de pagamento 4")
 			print(f"vc escolheu {tira} ")
 	elif pergunta3==5:
-			tira = tipopagamento.get("Forma de pagamento 5")
+			tira = escolhasala.get("Forma de pagamento 5")
 			print(f"vc escolheu  {tira} ")
 	else:
 			print("inválido")
--- projeto/pagamento.py
+++ projeto/pagamento.py
@@ -12,7 +12,9 @@ class Pagamentocinema:
     def gerar_contaprapagar(self):
         codigo_padrao = 12345678901
         print(f"Tipo de pagamento : {self.pagamento},  código do pagamento : {codigo_padrao}, Valor pago : {self.valor}, filme escolhido : {self.filme}, assento escolhido : {self.n_assento},\  dia escolhido : {self.dia}, hora escolhida : {self.hora}")
-    
+        
+    def tipopagamento(self):
+
 
 #o que está dentro do parametro é argumento para o parametro código
 
