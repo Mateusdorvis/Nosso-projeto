d188f4b060fae4dfac209585c156803c9a429e05
Mateusdorvis <mateusdorvis004@gmail.com>
2024-06-27 14:25:54 -0300

    ok

--- projeto/pagamento.py
+++ projeto/pagamento.py
@@ -1,3 +1,27 @@
 
+def gerar_ingresso(filme):
+
+    hora_escolhida= int(input("O cliente deseja para qual horário comercial entre 8h e 22h ?"))
+
+    if hora_escolhida<=22 and hora_escolhida>=8:
+        print('ok')
+
+    elif hora_escolhida<8 or hora_escolhida>22:
+        print("digite uma hora que seja entre 8h e 22h")
+        return gerar_ingresso(filme)
+    else:
+    	print("invalído")
+    	return gerar_ingresso(filme)
+    
+    minutos = int(input(" O cliente deseja para qual minutos?"))
+
+    
+    		
+
+
+
+			
+   
+                
     
         
